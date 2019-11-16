5e3a40feca4c7814e9a5edeeb514d53500af8712
Gui Seek <guiseek@gmail.com>
2019-11-16 03:06:36 -0300

    html demo improvments

--- scripts/h2j.js
+++ scripts/h2j.js
@@ -4,7 +4,6 @@ String.prototype.toHtmlEntities = function() {
   });
 };
 
-
 /**
  * Create string from HTML entities
  */
@@ -16,107 +15,64 @@ String.fromHtmlEntities = function(string) {
 };
 
 const s = `
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { timer } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
-
-const domains = ['google.com'];
-
-
-const URL_REGEXP = /^(http?|https):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|web|app|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
-
-function validateUrl(c: FormControl) {
-  return URL_REGEXP.test(c.value) ? null : {
-    validateUrl: {
-      valid: false
-    }
-  };
-}
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = '@guiseek/ui-error-builder';
-  form: FormGroup;
-
-  commonForm: FormGroup;
-  extraForm: FormGroup;
-
-  messages = {
-    validateUrl: 'URL inválida'
-  };
-
-  checkingDomain = false;
-
-  constructor(
-    private fb: FormBuilder
-  ) {
-    this.form = this.fb.group({
-      name: ['', [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(10)
-      ]],
-      age: ['', [
-        Validators.required,
-        Validators.min(18),
-        Validators.max(100)
-      ]],
-      email: ['', [
-        Validators.required,
-        Validators.email
-      ]],
-      pattern: ['', [
-        Validators.required,
-        Validators.pattern('[a-zA-Z ]*')
-      ]]
-    });
-    this.extraForm = this.fb.group({
-      domain: ['', [
-        Validators.required
-      ], [this.alreadyDomain]],
-      url: ['', [
-        Validators.required,
-        validateUrl
-      ]],
-      email: ['', [
-        Validators.email,
-        Validators.minLength(1),
-        Validators.maxLength(100)
-      ]]
-    });
-  }
-
-  alreadyDomain = (control: FormControl) => {
-
-    return timer(400).pipe(
-      switchMap(async () => {
-        this.checkingDomain = true;
-        return new Promise((resolve, reject) => {
-          window.setTimeout(() => {
-            const exists = !!domains.includes(control.value);
-            if (exists) {
-              resolve({
-                alreadyInUse: {
-                  value: control.value
-                }
-              });
-            } else {
-              resolve(null);
-            }
-          }, 400);
-        });
-      }),
-      tap(() => {
-        this.checkingDomain = false;
-      })
-    );
-  }
-}`;
-
+<form [formGroup]="form">
+  <div class="card">
+    <label for="name">
+      Nome
+      <br />
+      <small>(required, minlength e maxlength)</small>
+    </label>
+    <input
+      type="text"
+      id="name"
+      formControlName="name"
+      #nameError="errorBuilder"
+    />
+    <div>{{ nameError.message }}</div>
+  </div>
+  <div class="card">
+    <label for="age">
+      Idade
+      <br />
+      <small>(required, min e max)</small>
+    </label>
+    <input
+      type="number"
+      id="age"
+      formControlName="age"
+      #ageError="errorBuilder"
+    />
+    <div>{{ ageError.message }}</div>
+  </div>
+  <div class="card">
+    <label for="email">
+      Email
+      <br />
+      <small>(required e email)</small>
+    </label>
+    <input
+      type="email"
+      id="email"
+      formControlName="email"
+      #emailError="errorBuilder"
+    />
+    <div>{{ emailError.message }}</div>
+  </div>
+  <div class="card">
+    <label for="email">
+      Letras
+      <br />
+      <small>(required, pattern)</small>
+    </label>
+    <input
+      type="text"
+      id="pattern"
+      [messages]="messages"
+      formControlName="pattern"
+      #patternError="errorBuilder"
+    />
+    <div>{{ patternError.message }}</div>
+  </div>
+</form>`;
 
 console.log(s.toHtmlEntities());
